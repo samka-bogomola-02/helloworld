99f1db072ef697ded84647510ba674fed09d5985
irissinka1920 <rospizdyaika@gmail.com>
2024-05-29 15:42:32 +0300

    homework 7

--- src/Main.java
+++ src/Main.java
@@ -1,66 +1,75 @@
 public class Main {
     public static void main(String[] args) {
-    // Задача 1. С помощью цикла for выведите в консоль все числа от 1 до 10.
-        for (int i = 1; i <= 10; i++){
+    // 1.
+        double total = 0, percent = 1.01;
+        int deposite = 15_000, month = 1;
+        while (total < 2_459_000){
+            total = (total + deposite) * percent;
+            System.out.printf("Месяц %s, сумма накоплений равна %.2f рублей.%n", month, total);
+            month++;
+        }
+        System.out.println();
+    // 2.
+        int i = 1;
+        while (i <= 10){
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println();
+        for (i = 10; i != 0; i--){
             System.out.print(i + " ");
         }
-        System.out.print("\n");
-    // Задача 2. С помощью цикла for выведите в консоль все числа от 10 до 1.
-        for (int i = 10; i != 0; i--){
-            System.out.print(i + " ");
+        System.out.println("\n");
+    // 3.
+        int population = 12_000_000, years = 10;
+        short birthRate = 17, mortality = 8;
+        short populationAfter = (short) (birthRate - mortality);
+        for (int year = 1; year <= years; year++){
+            population += (int) (((float) population / 1_000) * populationAfter);
+            System.out.println("Год " + year + " численность населения составляет " + population);
         }
-        System.out.print("\n");
-    // Задача 3. Выведите в консоль все четные числа от 0 до 17.
-        for (int i = 0; i <= 17; i = i + 2){
-            System.out.print(i + " ");
+    // 4.
+        int depositeVasya = 15_000, finalAmount = 12_000_000, monthVasya = 1;
+        double percentVasya = 1.07;
+        while (depositeVasya < finalAmount) {
+            depositeVasya *= percentVasya;
+            System.out.printf("\nМесяц %s, сумма накоплений равна %s рублей.", monthVasya, depositeVasya);
+            monthVasya++;
         }
-        System.out.print("\n");
-    // Задача 4. Выведите в консоль все числа от 10 до −10 от бо́льшего числа к меньшему.
-        for (int i = 10; i != -11; i--){
-            System.out.print(i + " ");
+        System.out.println();
+    // 5.
+        int DepositeVasya = 15_000, FinalAmount = 12_000_000, MonthVasya = 1;
+        double PercentVasya = 1.07;
+        while (DepositeVasya < FinalAmount) {
+            DepositeVasya *= PercentVasya;
+            if (MonthVasya % 6 == 0) {
+                System.out.printf("\nМесяц %s, сумма накоплений равна %s рублей.", MonthVasya, DepositeVasya);
+            }
+            MonthVasya++;
         }
-        System.out.print("\n");
-    /* Задача 5. Напишите программу, которая выводит в консоль все високосные года,
-    начиная с 1904 года до 2096. В консоль результат должен выводиться в формате:
-    «… год является високосным». */
-        for (int i = 1904; i <= 2096; i = i +4){
-            System.out.println(i + " год является высокосным.");
+    // 6.
+        DepositeVasya = 15_000;
+        int numberOfMonths = 12 * 9;
+        for (int q = 1; q <= numberOfMonths; q++){
+            DepositeVasya *= PercentVasya;
+            if (q % 6 == 0) {
+                System.out.printf("\nМесяц %s, сумма накоплений равна %s рублей.", q, DepositeVasya);
+            }
         }
-    /* Задача 6. Напишите программу, которая выводит в консоль последовательность чисел:
-    7 14 21 28 35 42 49 56 63 70 77 84 91 98 */
-        for (int i = 7; i <= 98; i = i +7){
-            System.out.print(i + " ");
+    // 7.
+        System.out.println("\n");
+        int firstFriday = 5;
+        for (int w = firstFriday; w < 31; w += 7){
+            System.out.println("Сегодня пятница, " + w + " число, необходимо подготовить отчёт.");
         }
-        System.out.print("\n");
-    /* Задача 7. Напишите программу, которая выводит в консоль последовательность чисел:
-    1 2 4 8 16 32 64 128 256 512 */
-        for (int i = 1; i <= 512; i = i * 2){
-            System.out.print(i + " ");
-        }
-        System.out.print("\n");
-    /* Задача 8. Посчитайте с помощью цикла for сумму годовых накоплений,
-    если каждый месяц вы будете откладывать по 29 000 рублей «в банку».
-    Выведите сумму накоплений за каждый месяц в консоль в формате: «Месяц …, сумма накоплений равна … рублей». */
-        int total = 0, accumulation = 29_000;
-        for (int month = 1; month <= 12; month++){
-            total += accumulation;
-            System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей." );
-        }
-        System.out.print("\n");
-    /* Задача 9. Перепишите решение задачи выше при условии, что деньги вы откладывать будете не «в банку»,
-    а в банк под проценты — 12% годовых. Выведите сумму накоплений за каждый месяц в консоль в формате:
-    «Месяц …, сумма накоплений равна … рублей». */
-        int total1 = 0, accumulation1 = 29_000;
-        double percent = 1.01;
-        for (int i = 1; i <= 12; i++){
-            total1 *= percent;
-            total1 += accumulation1;
-            System.out.println("Месяц " + i + ", сумма накоплений равна " + total1 + " рублей." );
-        }
-    /* Напишите программу, которая выводит в консоль таблицу умножения на 2:
-    2*1=2; 2*2=4; 2*3=6; 2*4=8; 2*5=10; 2*6=12; 2*7=14; 2*8=16; 2*9=18; 2*10=20 */
-        for (int i = 1; i <= 10; i++){
-            System.out.println("2 * " + i + " = " + 2 * i);
+        System.out.println();
+    // 8.
+        int period = 79, currentYear = 2024;
+        int lowerBorder = currentYear - 200, upperBorder = currentYear + 100;
+        for (int y = 0; y < upperBorder; y += period){
+            if (y > lowerBorder){
+                System.out.println(y);
+            }
         }
     }
 }
