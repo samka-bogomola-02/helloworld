c74027af6f132ce64a7cd920dbd42e28ed71c0b5
irissinka1920 <rospizdyaika@gmail.com>
2024-06-03 19:40:15 +0300

    homework 8

--- src/Main.java
+++ src/Main.java
@@ -1,75 +1,52 @@
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-    // 1.
-        double total = 0, percent = 1.01;
-        int deposite = 15_000, month = 1;
-        while (total < 2_459_000){
-            total = (total + deposite) * percent;
-            System.out.printf("Месяц %s, сумма накоплений равна %.2f рублей.%n", month, total);
-            month++;
-        }
-        System.out.println();
-    // 2.
-        int i = 1;
-        while (i <= 10){
-            System.out.print(i + " ");
-            i++;
-        }
-        System.out.println();
-        for (i = 10; i != 0; i--){
-            System.out.print(i + " ");
-        }
-        System.out.println("\n");
-    // 3.
-        int population = 12_000_000, years = 10;
-        short birthRate = 17, mortality = 8;
-        short populationAfter = (short) (birthRate - mortality);
-        for (int year = 1; year <= years; year++){
-            population += (int) (((float) population / 1_000) * populationAfter);
-            System.out.println("Год " + year + " численность населения составляет " + population);
-        }
-    // 4.
-        int depositeVasya = 15_000, finalAmount = 12_000_000, monthVasya = 1;
-        double percentVasya = 1.07;
-        while (depositeVasya < finalAmount) {
-            depositeVasya *= percentVasya;
-            System.out.printf("\nМесяц %s, сумма накоплений равна %s рублей.", monthVasya, depositeVasya);
-            monthVasya++;
-        }
-        System.out.println();
-    // 5.
-        int DepositeVasya = 15_000, FinalAmount = 12_000_000, MonthVasya = 1;
-        double PercentVasya = 1.07;
-        while (DepositeVasya < FinalAmount) {
-            DepositeVasya *= PercentVasya;
-            if (MonthVasya % 6 == 0) {
-                System.out.printf("\nМесяц %s, сумма накоплений равна %s рублей.", MonthVasya, DepositeVasya);
-            }
-            MonthVasya++;
-        }
-    // 6.
-        DepositeVasya = 15_000;
-        int numberOfMonths = 12 * 9;
-        for (int q = 1; q <= numberOfMonths; q++){
-            DepositeVasya *= PercentVasya;
-            if (q % 6 == 0) {
-                System.out.printf("\nМесяц %s, сумма накоплений равна %s рублей.", q, DepositeVasya);
+    /* Задача 1. Объявите три массива:
+    1.1 Целочисленный массив, заполненный тремя цифрами — 1, 2 и 3 — с помощью ключевого слова new.*/
+        int[] w = new int[3];
+        w[0] = 1;
+        w[1] = 2;
+        w[2] = 3;
+    // 1.2 Массив, в котором можно хранить три дробных числа — 1.57, 7.654, 9.986. Массив сразу заполните значениями.
+        double[] q = new double[] {1.57, 7.754, 9.986};
+    /* 1.3 Произвольный массив. Тип и количество данных определите сами.
+    Самостоятельно выберите способ создания массива: с помощью ключевого слова или сразу заполненный элементами.*/
+        boolean[] e = new boolean[] {true, false, true, true};
+    /* Задача 2. Распечатайте на отдельной строчке элементы каждого массива по порядку через запятую.
+    В конце строки запятую ставить не надо.*/
+        System.out.println(w[0] + ", " + w[1] + ", " + w[2]);
+        System.out.println(q[0] + ", " + q[1] + ", " + q[2]);
+        System.out.println(e[0] + ", " + e[1] + ", " + e[2] + ", " + e[3]);
+    /* Задача 3. Распечатайте на отдельной строчке элементы каждого массива в обратном порядке через запятую.
+    В конце строки запятую ставить не надо.*/
+        for (int i = w.length - 1; i >= 0; i--){
+            if (i == 0){
+                System.out.println(w[i]);
+            } else {
+            System.out.print(w[i] + ", ");
             }
         }
-    // 7.
-        System.out.println("\n");
-        int firstFriday = 5;
-        for (int w = firstFriday; w < 31; w += 7){
-            System.out.println("Сегодня пятница, " + w + " число, необходимо подготовить отчёт.");
-        }
-        System.out.println();
-    // 8.
-        int period = 79, currentYear = 2024;
-        int lowerBorder = currentYear - 200, upperBorder = currentYear + 100;
-        for (int y = 0; y < upperBorder; y += period){
-            if (y > lowerBorder){
-                System.out.println(y);
+        for (int i = q.length - 1; i >= 0; i--){
+            if (i == 0){
+                System.out.println(q[i]);
+            } else {
+                System.out.print(q[i] + ", ");
             }
         }
+        for (int i = e.length - 1; i >= 0; i--){
+            if (i == 0){
+                System.out.println(true);
+            } else {
+                System.out.print(e[i] + ", ");
+            }
+        }
+    /* Задача 4. Пройдитесь по первому целочисленному массиву
+    и все нечетные числа в нем сделайте четными (нужно прибавить 1).*/
+        for (int i = 0; i < w.length; i++){
+            if (w[i] % 2 != 0){
+                w[i]++;
+            }
+        }
+        System.out.print(w[0] + ", " + w[1] + ", " + w[2]);
     }
 }
