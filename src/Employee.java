0e21107476df3df1621d5471f0b092b307cc37b6
irissinka1920 <rospizdyaika@gmail.com>
2024-06-26 21:51:07 +0300

    CW1

---
+++ src/Employee.java
@@ -0,0 +1,76 @@
+import java.util.Objects;
+
+public class Employee {
+    private String name;
+    private String lastname;
+    private String patronymic;
+    private int salary;
+    private int secession;
+    private int id;
+    private static int nextId = 1;
+    public Employee(String name, String lastname, String patronymic, int salary, int secession) {
+        this.name = name;
+        this.lastname = lastname;
+        this.patronymic = patronymic;
+        this.salary = salaryCheck(salary);
+        this.secession = secessionCheck(secession);
+        this.id = nextId++;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getLastname() {
+        return lastname;
+    }
+    public String getPatronymic() {
+        return patronymic;
+    }
+    public int getSalary() {
+        return salary;
+    }
+    public void setSalary(int salary) { //set
+        this.salary = salaryCheck(salary);
+    }
+    public int getSecession() {
+        return secession;
+    }
+    public void setSecession(int secession) { // set
+        this.secession = secessionCheck(secession);
+    }
+    public int getId() {
+        return id;
+    }
+    public boolean equals(Object o) {
+        if (this == o){
+            return true;
+        } else if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Employee employee = (Employee) o;
+        return id == employee.id && secession == employee.secession &&
+                Double.compare(employee.salary, salary) == 0 &&
+                Objects.equals(lastname, employee.lastname) &&
+                Objects.equals(name, employee.name) &&
+                Objects.equals(patronymic, employee.patronymic);
+    }
+    public int hashCode() {
+        return Objects.hash(id, name, lastname, patronymic, secession, salary);
+    }
+    public String toString() {
+        return "id " + id + ", employee " + name + " " + lastname + " " + patronymic +
+                ", department " + secession + ", salary " + salary;
+    }
+    //  Условия для зп и департаментов.
+    private int salaryCheck(int salary) {
+        if (salary < 0) {
+            throw new IllegalArgumentException("salary is a negative number");
+        }
+        return salary;
+    }
+    private int secessionCheck(int secession) {
+        if (secession < 1 || secession > 5) {
+            throw new IllegalArgumentException("departments cannot be less than one and more than five");
+        }
+        return secession;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,72 @@
+public class Main {
+    public Employee[] employees;
+    public Main() {
+        employees = new Employee[10];
+        initializeEmployees();
+        System.out.println(" ");
+    }
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.printEmployees();
+        System.out.println(" ");
+        System.out.println("Total cost of salary per month: " + main.getTotalSalary());
+        System.out.println("Employee with minimum salary: " + main.getEmployeeWithMinSalary());
+        System.out.println("Employee with maximum salary: " + main.getEmployeeWithMaxSalary());
+        System.out.println("Average salary per month: " + main.totalMonthlyAverageSalary());
+        System.out.println(" ");
+        System.out.println("A list of employees: ");
+        main.printEmployeesFullNames();
+    }
+    public void initializeEmployees() {
+        employees[0] = new Employee("Анато́лий", "Оноприе́нко", "Ю́рьевич", 60_500, 2);
+        employees[1] = new Employee("Алекса́ндр", "Пичу́шкин", "Ю́рьевич", 47_000, 1);
+        employees[2] = new Employee("Михаил", "Попко́в", "Ви́кторович", 62_000, 5);
+        employees[3] = new Employee("Андре́й", "Чикати́ло", "Рома́нович", 140_000, 4);
+        employees[4] = new Employee("Сергей", "Ткач", "Фёдорович", 115_000, 5);
+        employees[5] = new Employee("Алексей", "Выговский", "Олегович", 95_000, 3);
+        employees[6] = new Employee("Теодо́р", "Ба́нди", "Ро́берт", 82_000, 4);
+        employees[7] = new Employee("Ра́дик", "Таги́ров", "Таги́рович", 125_500, 2);
+        employees[8] = new Employee("Да́рья", "Салтыко́ва", "Никола́евна", 110_000, 3);
+        employees[9] = new Employee("Пе́дро", "Ло́пес", "Ало́нсо", 75_000, 1);
+    }
+    public void printEmployees() {
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+    }
+    public int getTotalSalary() {
+        int totalSalary = 0;
+        for (Employee employee : employees) {
+          totalSalary += employee.getSalary();
+        }
+        return totalSalary;
+    }
+    public Employee getEmployeeWithMinSalary() {
+        Employee minSalaryEmployee = employees[0];
+        for (Employee employee : employees) {
+            if (employee.getSalary() < minSalaryEmployee.getSalary()) {
+                minSalaryEmployee = employee;
+            }
+        }
+        return minSalaryEmployee;
+    }
+    public Employee getEmployeeWithMaxSalary() {
+        Employee employeeWithMaxSalary = employees[0];
+        for (Employee employee : employees) {
+            if (employee.getSalary() > employeeWithMaxSalary.getSalary()) {
+                employeeWithMaxSalary = employee;
+            }
+        }
+        return employeeWithMaxSalary;
+    }
+    public int totalMonthlyAverageSalary() {
+        return getTotalSalary() / employees.length;
+    }
+    public void printEmployeesFullNames() {
+        int counter = 1;
+        for (Employee employee : employees) {
+            System.out.println(counter + ", " + employee.getLastname() + " " + employee.getName() + " " + employee.getPatronymic());
+            counter++;
+        }
+    }
+}
